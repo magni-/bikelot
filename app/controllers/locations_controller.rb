86332d9d307dabbdd27e0006844114fba245e712
jpcolomer <jpcolomer@gmail.com>
2013-10-05 13:49:06 -0300

    defines LocationsController#create

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,18 @@
 class LocationsController < ApplicationController
-	def create
-		
+
+	#POST
+  #params: lat, long
+  def create
+    location = Location.new(location_params)
+    if location.save
+      render json: {success: true}, status: :created
+    else
+      render json: {success: false}
+    end
 	end
+  
+  private
+  def location_params
+    params.require(:location).permit(:lat, :long)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Bikelot::Application.routes.draw do
   root to: "static#index"
+  resources :locations, only: :create
 end
