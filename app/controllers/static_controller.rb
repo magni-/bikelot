ccb24a79d407ec13c94086aed53219f482eccf14
Feña Agar <ferliagno@gmail.com>
2013-10-05 16:42:49 -0300

    gmaps4rails

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,5 @@
 class StaticController < ApplicationController
   def index
+    @json = Location.all.to_gmaps4rails
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,15 @@
 class Location < ActiveRecord::Base
-	validates :lat, :long, presence: true, numericality: {
+	acts_as_gmappable lat: :lat, lng: :long
+
+  attr_accessor :gmaps, :street, :city, :country
+
+  validates :lat, :long, presence: true, numericality: {
     greater_than: -90,
     less_than: 90
   }
+
+  def gmaps4rails_address
+    #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
+    "#{self.street}, #{self.city}, #{self.country}"
+  end
 end
